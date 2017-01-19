b80707cac24c17da6da5db2d9a8ee142ac9dea25
Jufebrown <jufebrown@gmail.com>
2017-01-19 11:48:59 -0600

    added functionality to weather controller

--- app.js
+++ app.js
@@ -11,9 +11,17 @@ angular
         templateUrl: '/partials/weather.html'
       })
   })
-  .controller('RootCtrl', function() {
+  .controller('RootCtrl', function($scope, $location) {
     console.log('I am a root controller')
+    $scope.gotoWeather = () => {$location.url(`/weather/${$scope.zip}`)}
   })
-  .controller('WeatherCtrl', function() {
+  .controller('WeatherCtrl', function($http, $routeParams, $scope) {
     console.log('I am a weather controller')
+    $http.get(`http://api.wunderground.com/api/35084d7263fd2e96/conditions/q/${$routeParams.zipcode}.json`)
+    .then((response) => {
+      return response.data.current_observation.temp_f
+    })
+    .then((temp) => {
+      $scope.temperature = temp
+    })
   })
