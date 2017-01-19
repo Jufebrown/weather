e2b4f5e6b691658cf25d78dfe3e0f786b3089df5
Jufebrown <jufebrown@gmail.com>
2017-01-19 12:09:18 -0600

    took http request out of controller and put it in factory

--- app.js
+++ app.js
@@ -15,13 +15,26 @@ angular
     console.log('I am a root controller')
     $scope.gotoWeather = () => {$location.url(`/weather/${$scope.zip}`)}
   })
-  .controller('WeatherCtrl', function($http, $routeParams, $scope) {
+  .controller('WeatherCtrl', function(weatherFactory, $routeParams, $scope) {
     console.log('I am a weather controller')
-    $http.get(`http://api.wunderground.com/api/35084d7263fd2e96/conditions/q/${$routeParams.zipcode}.json`)
-    .then((response) => {
-      return response.data.current_observation.temp_f
-    })
-    .then((temp) => {
-      $scope.temperature = temp
-    })
+    weatherFactory
+      .getWeather($routeParams.zipcode)
+      .then((weather) => {
+        $scope.temperature = weather.temp
+        $scope.city = weather.city
+      })
+  })
+  .factory('weatherFactory', ($http) => {
+    return {
+      getWeather (zipcode) {
+        return $http
+          .get(`http://api.wunderground.com/api/35084d7263fd2e96/conditions/q/${zipcode}.json`)
+          .then((response) => {
+            return {
+              temp: response.data.current_observation.temp_f,
+              city: response.data.current_observation.display_location.full,
+            }
+          })
+      },
+    }
   })
