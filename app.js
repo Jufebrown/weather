1ae5c66a9350a749a86c352eae33bbfc0bdf0841
Jufebrown <jufebrown@gmail.com>
2017-01-19 10:58:45 -0600

    added weather route and controller

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 angular
   .module('anguweather', ['ngRoute'])
   .config(($routeProvider) => {
-    $routeProvider.when('/', {
-      controller: 'RootCtrl',
-      templateUrl: 'partials/root.html'
-    })
-
+    $routeProvider
+      .when('/', {
+        controller: 'RootCtrl',
+        templateUrl: 'partials/root.html'
+      })
+      .when('/weather/:zipcode', {
+        controller: 'WeatherCtrl',
+        templateUrl: '/partials/weather.html'
+      })
   })
   .controller('RootCtrl', function() {
     console.log('I am a root controller')
+  })
+  .controller('WeatherCtrl', function() {
+    console.log('I am a weather controller')
   })
